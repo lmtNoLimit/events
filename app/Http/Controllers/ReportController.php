945b2e1707e6499844e6fa4ebea07b369cf956f5
Lâm Minh Thành <lmt151099@gmail.com>
2019-11-16 00:46:27 +0700

    Room overview, show chart for an event (room capacity)

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,34 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event;
+use App\Attendee;
+use App\Session;
+use App\Room;
 use DB;
 
 class ReportController extends Controller
 {
     public function Chartjs($eventId) {
-        $sessions = DB::table('sessions')
-            // ->join("session_registrations", "sessions.id", "=", "session_registrations.session_id")
-            // ->join('registrations', "registrations.id", "=", "session_registrations.registration_id")
-            // ->join('attendees', "attendees.id", "=", "registrations.attendee_id")
-            ->join('rooms', "rooms.id", "=", "sessions.room_id")
+        $event = Event::where("id", $eventId)->first();
+        $data = Session::join('rooms', "rooms.id", "=", "sessions.room_id")
             ->join("channels", "channels.id", "=", "rooms.channel_id")
             ->join("events", "events.id", "=", "channels.event_id")
-            // ->select("sessions.title")
             ->where("events.id", $eventId)
-            ->select("sessions.title")
-            ->distinct()
+            ->select("sessions.id", "sessions.title")
             ->get();
-        $attendees = DB::table('attendees')
-            ->join("registrations", "registrations.attendee_id", "=", "attendees.id")
-            ->join("session_registrations", "session_registrations.registration_id", "=", "registrations.id")
-            ->join("sessions", "sessions.id", "=", "session_registrations.session_id")
-            ->join('rooms', "rooms.id", "=", "sessions.room_id")
-            ->join("channels", "channels.id", "=", "rooms.channel_id")
-            ->join("events", "events.id", "=", "channels.event_id")
-            ->select("sessions.id as session_id", DB::raw("count('attendees.id') as attendees_count"))
-            ->where("events.id", $eventId)
-            ->groupBy("sessions.id")
-            ->get();
-        return response()->json($attendees);
+        
+        foreach ($data as $key => $session) {
+            $attendees = Attendee::join("registrations", "registrations.attendee_id", "=", "attendees.id")
+                ->join("session_registrations", "session_registrations.registration_id", "=", "registrations.id")
+                ->join("sessions", "sessions.id", "=", "session_registrations.session_id")
+                ->select(DB::raw("count('attendees.id') as attendee_count"))
+                ->where("sessions.id", $session->id)
+                ->first();
+            $rooms = Room::join("sessions", "sessions.room_id", "=", "rooms.id")
+                ->where("sessions.id", $session->id)
+                ->select("rooms.capacity")
+                ->first();
+            $session->attendee_count = $attendees->attendee_count;
+            $session->room_capacity = $rooms->capacity;
+        }
+        return view('reports/index', [
+            "user" => auth()->user(),
+            "event" => $event,
+            "data" => $data
+        ]);
+        // return response()->json(["data" => $data]);
     }
 }
--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -15,15 +15,7 @@
 </head>
 
 <body>
-<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="events/index.html">Event Platform</a>
-    <span class="navbar-organizer w-100">{insert organization name}</span>
-    <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" id="logout" href="index.html">Sign out</a>
-        </li>
-    </ul>
-</nav>
+@include('partials.navbar')
 
 <div class="container-fluid">
     <div class="row">
@@ -34,7 +26,7 @@
                 </ul>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>{insert event name}</span>
+                    <span>{{$event->name}}</span>
                 </h6>
                 <ul class="nav flex-column">
                     <li class="nav-item"><a class="nav-link" href="events/detail.html">Overview</a></li>
@@ -52,9 +44,9 @@
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
             <div class="border-bottom mb-3 pt-3 pb-2">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-                    <h1 class="h2">{insert event name}</h1>
+                    <h1 class="h2">{{$event->name}}</h1>
                 </div>
-                <span class="h6">{insert event date}</span>
+                <span class="h6">{{date("F j, Y", strtotime($event->date))}}</span>
             </div>
 
             <div class="mb-3 pt-3 pb-2">
@@ -68,13 +60,32 @@
             <div class="row">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <canvas id="canvas" height="280" width="600"></canvas>
+                        <canvas id="canvas" height="400" width="900"></canvas>
                     </div>
                 </div>
             </div>
             <script>
+                let data = {!! $data !!}
+                let sessions = data.map(item => item.title);
+                let attendeeBgColor = data.map(item => {
+                    if(item.attendee_count > item.room_capacity) {
+                        return 'rgba(201, 44, 65, 0.93)';
+                    }
+                    return 'rgba(173, 235, 153, 1)';
+                });
                 let barChartData = {
-                    
+                    labels: sessions,
+                    datasets: [{
+                        label: 'Attendees',
+                        backgroundColor: attendeeBgColor,
+                        borderWidth: 1,
+                        data: data.map(item => item.attendee_count)
+                    }, {
+                        label: 'Capacity',
+                        backgroundColor: 'rgba(153, 187, 235, 1)',
+                        borderWidth: 1,
+                        data: data.map(item => item.room_capacity)
+			        }]
                 }
                 window.onload = function() {
                     var ctx = document.getElementById('canvas').getContext('2d');
@@ -84,17 +95,12 @@
                         options: {
                             responsive: true,
                             legend: {
-                                position: 'top',
+                                position: 'right',
                             },
-                            title: {
-                                display: true,
-                                text: 'Chart.js Bar Chart'
-                            }
                         }
                     });
                 };
             </script>
-
         </main>
     </div>
 </div>
